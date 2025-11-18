b39f1b9e0ddb34482dceca1043cd008fc4ef830c
João Victor RP <jvrp1000@gmail.com>
2025-11-18 08:39:35 -0300

    Refactor resolverMochila to use arrays for pesos and valores

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-int *pesos, *valores;
-
 int max(int a, int b){
         if(a < b){
                 return b;
@@ -11,7 +9,7 @@ int max(int a, int b){
 }
 
 
-int resolverMochila(int ind, int pesoAtual, int n, int m){
+int resolverMochila(int ind, int pesoAtual, int n, int m, int pesos[], int valores[]){
         //caso base:
         //aqui se já consideramos todos os itens ind == n
         //não precisa adicionar mais nada
@@ -20,7 +18,7 @@ int resolverMochila(int ind, int pesoAtual, int n, int m){
 
         //1° opcao: não incluir o ind
         //passando para o próximo item, mantendo o mesmo peso
-        int valorSemItem = resolverMochila(ind + 1, pesoAtual, n, m);
+        int valorSemItem = resolverMochila(ind + 1, pesoAtual, n, m, pesos, valores);
 
         //2° opcao: incluir o ind
         int valorComItem = 0; // valor padrão se nao puder incluir
@@ -29,7 +27,7 @@ int resolverMochila(int ind, int pesoAtual, int n, int m){
         if((pesoAtual + pesos[ind]) <= m);
                 //se couber, o valor é: o valor deste item +
                 //o valor maximo que podemos obter do resto dos itens
-                valorComItem = valores[ind] + resolverMochila(ind + 1, pesoAtual + pesos[ind], n, m);
+                valorComItem = valores[ind] + resolverMochila(ind + 1, pesoAtual + pesos[ind], n, m, pesos, valores);
 
         return max(valorSemItem, valorComItem);
 }
@@ -37,13 +35,17 @@ int resolverMochila(int ind, int pesoAtual, int n, int m){
 int main(){
 
         int n, m;
-        scanf("%d %d", &n, &m);
+        int pesos[20];
+        int valores[20];
+        scanf("%d", &n, &m);
 
         for(int i = 0; i < n - 1; i++){
                 scanf("%d %d", pesos[i], valores[i]);
         }
 
-        int res = resolverMochila(0, 0, n, m);
+        int res = resolverMochila(0, 0, n, m, pesos, valores);
+        
+        printf("%d", res);
 
         return 0;
 }
